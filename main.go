45d10e58f6d5e1fed436199e0ca43dd7afb33b9c
drazen-todorovic <drazen.todorovic@gmail.com>
2018-11-11 12:00:01 +0100

    Added flags for port and interval

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
@@ -9,15 +10,29 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+	interval int
+	port     int
+)
+
+func init() {
+	flag.IntVar(&interval, "interval", 1, "Interval of collecting metrics (seconds)")
+	flag.IntVar(&port, "port", 2112, "Listening port")
+	flag.Parse()
+}
+
 func main() {
 
-	go util.RunMetricWorker(1)
+	go util.RunMetricWorker(interval)
 
 	http.Handle("/metrics", promhttp.Handler())
 
-	fmt.Println("Prometheus netstat exproter is running on http://0.0.0.0:2112")
+	fmt.Println(fmt.Sprintf("Collecting interval: %v seconds", interval))
+	fmt.Println(fmt.Sprintf("Running on http://:%v", port))
 
-	err := http.ListenAndServe(":2112", nil)
+	addr := fmt.Sprintf(":%v", port)
+
+	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		panic(err)
 	}
